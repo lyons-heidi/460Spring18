2fce3dd42f96adac75570c1e96c861de1655dfc3
Heidi Lyons <heidi.r.lyons@wsu.edu>
2018-04-24 16:03:20 -0700

    start of cat implementation
    Signed-off-by: Heidi Lyons <heidi.r.lyons@wsu.edu>

---
+++ LAST/cat.c
@@ -0,0 +1,48 @@
+#include "ucode.c"
+
+void * my_memset(void *s, int c, int n);
+
+int main (int argc, char *argv[]) {
+    prints("********** Heidi's Cat MEOW ************ \n");
+    
+    int n = 0;
+    int fd;
+    stdin = 0, stdout = 1;
+    char buf[1024], cmdLine[64], user_ch;
+    
+    // make sure user enters input
+    if(argc < 0){
+        prints("Error, no input found! \n");
+        exit(1);
+    }
+
+    // Only one ch found
+    else if (argc == 1) {
+        fd = 0;
+        
+        while(read(0, &user_ch, 1)){
+            // If newline detected, write to screen
+            if( user_ch == '\n' || user_ch == '\r' )
+            {
+                write(1, "\n", 1);
+                write(1, "\r", 1);
+            }
+
+            // write char to screen
+            else{
+                write(1, &user_ch, 1);
+            }
+        }
+    }
+
+    else{
+        // open file, read and print the buffer
+        fd = open(argv[1], O_RDONLY);
+        my_memset(buf, 0, 1024);
+        while((n = read( fd, buf, 1024))){
+            prints(buf);
+            my_memset(buf, 0, 1024);
+        }
+        close(fd);
+    }
+}
